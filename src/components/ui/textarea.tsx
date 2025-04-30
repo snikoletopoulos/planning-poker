137c87f359956b47aa278dae35335538ac37beea
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-05-01 01:03:36 +0200

    Fix eslint error

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,12 +3,11 @@ import type { Ref } from "react";
 import { cn } from "@/lib/styles/utils";
 
 export interface TextareaProps
-	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+	ref?: Ref<HTMLTextAreaElement>;
+}
 
-export const Textarea = ({
-	className,
-	...props
-}: TextareaProps & { ref?: Ref<HTMLTextAreaElement> }) => (
+export const Textarea = ({ className, ...props }: TextareaProps) => (
 	<textarea
 		className={cn(
 			"border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
