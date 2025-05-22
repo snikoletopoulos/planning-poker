12af5027dc5e473dcb846603ecedeea7712757d9
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-05-22 23:37:14 +0200

    Listen to websocket events

--- web/src/app/room/[id]/_components/RoomContext.tsx
+++ web/src/app/room/[id]/_components/RoomContext.tsx
@@ -6,9 +6,11 @@ import {
 	useContext,
 	useEffect,
 	useMemo,
+	useRef,
 	useState,
 	type PropsWithChildren,
 } from "react";
+import { z } from "zod";
 
 import { useCurrentUser } from "@/components/CurrentUserProvider";
 import type { Member, Room, Story, Vote } from "@/lib/db/schema";
@@ -18,58 +20,51 @@ import {
 } from "../_actions/stories";
 
 interface StoryWithVotes extends Story {
-	votes: Vote[];
+	votes: (Omit<Vote, "vote"> & { vote: number | null })[];
 }
 
 interface RoomContextData {
 	room: Room;
 	stories: StoryWithVotes[];
-	members: Member[];
+	members: Pick<Member, "id" | "name">[];
 	activeStory: StoryWithVotes;
 	changeActiveStory: (storyId: Story["id"]) => void;
 	selectedCard: number | "?" | null;
 	selectCard: (card: number | "?" | null) => void;
 	completeStory: () => void;
 	isLiveUpdating: boolean;
+	nextStory: () => void;
 }
 
 const RoomContext = createContext<RoomContextData | null>(null);
 
 export const RoomProvider = ({
 	room,
-	stories,
-	members,
+	stories: initialStories,
+	members: initialMembers,
 	children,
 }: PropsWithChildren<{
 	room: Room;
 	stories: StoryWithVotes[];
-	members: Member[];
+	members: Pick<Member, "id" | "name">[];
 }>) => {
 	const currentUser = useCurrentUser();
+	const ws = useRef<WebSocket | null>(null);
+
+	const [stories, setStories] = useState(initialStories);
+	const [members, setMembers] = useState(initialMembers);
 
 	const [activeStory, setActiveStory] = useState(
 		() => stories.find(story => !story.isCompleted) ?? stories[0],
 	);
-	const [selectedCard, setSelectedCard] = useState<number | "?" | null>(null);
+	const [selectedCard, setSelectedCard] = useState<number | "?" | null>(() => {
+		const userVote = activeStory.votes.find(
+			vote => vote.memberId === currentUser.id,
+		);
+		if (!userVote) return null;
+		return userVote.vote ? +userVote.vote : "?";
+	});
 	const [isLiveUpdating, setIsLiveUpdating] = useState(false);
-
-	useEffect(() => {
-		const socket = new WebSocket("ws://localhost:8080/");
-
-		socket.addEventListener("open", () => setIsLiveUpdating(true));
-		socket.addEventListener("close", () => setIsLiveUpdating(false));
-		socket.addEventListener("error", error => {
-			console.error("🪚 error", error);
-			setIsLiveUpdating(false);
-		});
-
-		socket.addEventListener("message", event => {
-			const data = JSON.parse(event.data);
-			console.log("🪚 data:", data);
-		});
-
-		return () => socket.close();
-	}, []);
 
 	const changeActiveStory = useCallback(
 		(storyId: Story["id"]) => {
@@ -106,6 +101,111 @@ export const RoomProvider = ({
 		[activeStory.id],
 	);
 
+	const nextStory = useCallback(() => {
+		if (!ws.current) return;
+		ws.current.send(JSON.stringify({ action: "next_story" }));
+	}, []);
+
+	useEffect(() => {
+		const socket = new WebSocket(
+			`${process.env.NEXT_PUBLIC_WS_PROTOCOL}://${process.env.NEXT_PUBLIC_WS_URI}`,
+			["Authentication", "test"],
+		);
+
+		socket.addEventListener("open", () => setIsLiveUpdating(true));
+		socket.addEventListener("close", () => setIsLiveUpdating(false));
+		socket.addEventListener("error", error => {
+			console.error("WebSocket Error", error);
+			setIsLiveUpdating(false);
+			socket.send("leave");
+			socket.close();
+		});
+
+		socket.addEventListener("message", event => {
+			const WsEventSchema = z.discriminatedUnion("action", [
+				z.object({
+					action: z.literal("user_voted"),
+					storyId: z.string(),
+					memberId: z.string(),
+				}),
+				z.object({
+					action: z.literal("member_joined"),
+					member: z.object({
+						id: z.string(),
+						name: z.string(),
+					}),
+				}),
+				z.object({
+					action: z.literal("new_story"),
+					story: z.object({
+						id: z.string(),
+						title: z.string(),
+						description: z.string(),
+						isCompleted: z.boolean(),
+						roomId: z.string(),
+						createdAt: z.string(),
+					}),
+				}),
+				z.object({ action: z.literal("next_story") }),
+			]);
+
+			const data = WsEventSchema.parse(JSON.parse(event.data as string));
+
+			switch (data.action) {
+				case "user_voted": {
+					setStories(stories => {
+						const story = stories.find(story => story.id === data.storyId);
+						if (!story) return stories;
+						const exitingVoteIndex = story.votes.findIndex(
+							vote => vote.memberId === data.memberId,
+						);
+
+						if (exitingVoteIndex === -1) {
+							story.votes.push({
+								vote: null,
+								storyId: data.storyId,
+								memberId: data.memberId,
+								createdAt: new Date(),
+							});
+						}
+
+						return [...stories];
+					});
+					break;
+				}
+				case "member_joined": {
+					setMembers(members => [...members, data.member]);
+					break;
+				}
+				case "new_story": {
+					setStories(stories => [
+						...stories,
+						{
+							...data.story,
+							createdAt: new Date(data.story.createdAt),
+							votes: [],
+						},
+					]);
+					break;
+				}
+				case "next_story": {
+					const nextStory = stories.find(story => !story.isCompleted);
+					if (!nextStory) return;
+					changeActiveStory(nextStory.id);
+					break;
+				}
+			}
+		});
+
+		ws.current = socket;
+
+		return () => {
+			if (socket.readyState !== WebSocket.OPEN) return;
+			socket.send(JSON.stringify({ action: "leave" }));
+			socket.close();
+		};
+	}, []);
+
 	const value = useMemo(
 		() => ({
 			room,
@@ -117,6 +217,7 @@ export const RoomProvider = ({
 			selectCard,
 			completeStory,
 			isLiveUpdating,
+			nextStory,
 		}),
 		[
 			room,
@@ -128,6 +229,7 @@ export const RoomProvider = ({
 			selectCard,
 			completeStory,
 			isLiveUpdating,
+			nextStory,
 		],
 	);
 
