efd51bc8d6228e799cd30e9eac4a4f469c9d59e6
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-05-22 23:37:28 +0200

    Show badge with websocket connection status

--- web/src/app/room/[id]/_components/Header.tsx
+++ web/src/app/room/[id]/_components/Header.tsx
@@ -5,18 +5,25 @@ import Link from "next/link";
 
 import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
+import { cn } from "@/lib/styles/utils";
 import { useRoom } from "./RoomContext";
 
 export const Header = () => {
-	const { room, members } = useRoom();
+	const { room, members, isLiveUpdating } = useRoom();
 
 	return (
 		<header className="mb-8 flex items-center justify-between">
 			<div>
 				<h1 className="text-foreground text-3xl font-bold">{room.name}</h1>
 				<div className="mt-2 flex items-center">
-					<Badge variant="outline" className="mr-2">
-						Room: {room.id}
+					<Badge
+						variant="outline"
+						className={cn("mr-2", {
+							"bg-green-500 dark:bg-green-900": isLiveUpdating,
+							"bg-red-600 text-white dark:bg-red-900": !isLiveUpdating,
+						})}
+					>
+						{isLiveUpdating ? "Online" : "Offline"}
 					</Badge>
 
 					<Badge className="flex items-center">
