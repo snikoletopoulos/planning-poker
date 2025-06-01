3a645c09bc89f03ca578474b2079bf763cad0e54
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-06-02 01:28:45 +0200

    Change panic to a log statement for prod with docker

--- websocket/cmd/web/env.go
+++ websocket/cmd/web/env.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
 
 func setupEnvVars() {
 	if err := godotenv.Load("../web/.env"); err != nil {
-		panic("Error loading .env file")
+		fmt.Println("Didn't find .env file")
 	}
 }
