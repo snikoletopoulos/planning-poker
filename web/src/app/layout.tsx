15002bedb330e61832126147834751d0c36b35a2
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-01-30 21:32:13 +0100

    Fix theme controller

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: ReactNode }) => (
 	<html lang="en" suppressHydrationWarning>
 		<body className={inter.className}>
-			<ThemeProvider>
+			<ThemeProvider attribute="class">
 				<NavBar />
 				{children}
 				<Toaster />
