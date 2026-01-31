6df6f1e310ab96d795f979d9946c51b2bc672be6
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-01-31 04:07:46 +0100

    Format code

--- web/src/app/api/auth/[...all/route.ts
+++ web/src/app/api/auth/[...all/route.ts
@@ -1,4 +1,5 @@
-import { auth } from "@/lib/auth/auth.ts";
 import { toNextJsHandler } from "better-auth/next-js";
+
+import { auth } from "@/lib/auth/auth.ts";
 
 export const { POST, GET } = toNextJsHandler(auth);
