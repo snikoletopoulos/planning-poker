56a779475c4070b1ae009361b6df47d576fc62cd
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-01 14:56:01 +0100

    Wrap elements in form to handle enters'

--- web/src/app/(auth)/login/sign-in.tsx
+++ web/src/app/(auth)/login/sign-in.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/Card";
 import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/Label";
+import { Separator } from "@/components/ui/Separator";
 import { authClient } from "@/lib/auth/auth-client";
 
 export const SignIn = () => {
@@ -35,6 +36,7 @@ export const SignIn = () => {
 		await authClient.signIn.email({
 			email,
 			password,
+			callbackURL: "/",
 			fetchOptions: {
 				onRequest: () => {
 					setLoading(true);
@@ -55,56 +57,61 @@ export const SignIn = () => {
 				</CardDescription>
 			</CardHeader>
 
-			<CardContent className="space-y-4">
-				<div className="grid gap-2">
-					<Label htmlFor="email">Email</Label>
-					<Input
-						id="email"
-						type="email"
-						placeholder="john.doe@example.com"
-						required
-						onChange={event => {
-							setEmail(event.target.value);
-						}}
-						value={email}
-					/>
-				</div>
+			<CardContent>
+				<form action={handleLogin}>
+					<div className="space-y-4">
+						<div className="grid gap-2">
+							<Label htmlFor="email">Email</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder="john.doe@example.com"
+								required
+								onChange={event => {
+									setEmail(event.target.value);
+								}}
+								value={email}
+							/>
+						</div>
 
-				<div className="grid gap-2">
-					<Label htmlFor="password">Password</Label>
-					<Input
-						id="password"
-						type="password"
-						placeholder="password"
-						autoComplete="password"
-						value={password}
-						onChange={event => setPassword(event.target.value)}
-					/>
-				</div>
+						<div className="grid gap-2">
+							<Label htmlFor="password">Password</Label>
+							<Input
+								id="password"
+								type="password"
+								placeholder="password"
+								autoComplete="password"
+								value={password}
+								onChange={event => setPassword(event.target.value)}
+							/>
+						</div>
 
-				<Button className="w-full" disabled={loading} onClick={handleLogin}>
-					{loading ? <Loader2 size={16} className="animate-spin" /> : "Login"}
-				</Button>
+						<Button className="w-full" disabled={loading} onClick={handleLogin}>
+							{loading ? (
+								<Loader2 size={16} className="animate-spin" />
+							) : (
+								"Login"
+							)}
+						</Button>
+					</div>
 
-				<Button
-					variant="outline"
-					className="w-full gap-2"
-					disabled={loading}
-					onClick={handleLogin}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1em"
-						height="1em"
-						viewBox="0 0 24 24"
-					>
-						<path
-							fill="#00A4EF"
-							d="M2 3h9v9H2zm9 19H2v-9h9zM21 3v9h-9V3zm0 19h-9v-9h9z"
-						/>
-					</svg>
-					Sign in with Microsoft
-				</Button>
+					<Separator className="my-6" />
+
+					<Button variant="outline" className="w-full gap-2" disabled={loading}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="1em"
+							height="1em"
+							viewBox="0 0 24 24"
+						>
+							<path
+								fill="#00A4EF"
+								d="M2 3h9v9H2zm9 19H2v-9h9zM21 3v9h-9V3zm0 19h-9v-9h9z"
+							/>
+						</svg>
+						Sign in with Microsoft
+					</Button>
+				</form>
 			</CardContent>
 		</>
 	);
