359593409c7c63fc4ca37de9d468bd7b6e6ef906
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-01 13:18:09 +0100

    Fix eslint error

--- web/src/components/ui/Toasts.tsx
+++ web/src/components/ui/Toasts.tsx
@@ -11,6 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 	return (
 		<Sonner
 			theme={theme as ToasterProps["theme"]}
+			// eslint-disable-next-line better-tailwindcss/no-unknown-classes
 			className="toaster group"
 			toastOptions={{
 				classNames: {
